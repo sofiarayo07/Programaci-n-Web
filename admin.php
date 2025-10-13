fb33533f0ca35d93d6cb40f652e984447ac99e69
AlcorF91 <rauru.z72@gmail.com>
2025-10-13 00:28:14 -0600

    correccion de errores

--- admin.php
+++ admin.php
@@ -1,6 +1,5 @@
 <?php
-// admin.php
-// (En el futuro aquí podrías validar la sesión del usuario)
+
 ?>
 <!doctype html>
 <html lang="es">
@@ -74,7 +73,7 @@
       </div>
     </footer>
 
-    <!-- Scripts -->
+   
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
